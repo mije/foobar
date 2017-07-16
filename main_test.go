7886654b35f311a27f752982fe434aa622d34ac0
Michal Jemala <michal.jemala@gmail.com>
2017-07-16 23:09:11 +0200

    Add DB migrations for tests

--- main_test.go
+++ main_test.go
@@ -1,12 +1,62 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"os"
 	"testing"
+
+	"github.com/mattes/migrate"
 )
+
+func init() {
+	mustGetenv := func(key string) string {
+		val := os.Getenv(key)
+		if val == "" {
+			log.Fatalf("test: init: unable to find %q", key)
+		}
+		return val
+	}
+	m, err := migrate.New(mustGetenv("DB_MIGRATIONS"), mustGetenv("DB_URL"))
+	if err != nil {
+		log.Fatalf("test: init: %v", err)
+	}
+
+	if err := m.Up(); err != nil {
+		log.Fatalf("test: init: %v", err)
+	}
+}
 
 func TestEnv(t *testing.T) {
 	dbURL := os.Getenv("DB_URL")
-	log.Printf("DB_URL=%q", dbURL)
+	if dbURL == "" {
+		t.Fatal("unable to find \"DB_URL\"")
+	}
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT * FROM user")
+	if err != nil {
+		t.Fatal(err)
+	}
+	for rows.Next() {
+		var (
+			id        int
+			firstName string
+			lastName  string
+		)
+		err = rows.Scan(&id, &firstName, &lastName)
+		if err != nil {
+			t.Fatal(err)
+		}
+		fmt.Printf("%v | %v | %v\n", id, firstName, lastName)
+	}
+	if err := rows.Err(); err != nil {
+		t.Fatal(err)
+	}
 }
