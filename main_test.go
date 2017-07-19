21f9c4327fd18b17e59299a1bc1675796b69e21b
Michal Jemala <michal.jemala@gmail.com>
2017-07-19 14:24:31 +0200

    Amend integ. tests

--- main_test.go
+++ main_test.go
@@ -5,35 +5,11 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"log"
 	"os"
 	"testing"
 
 	_ "github.com/lib/pq"
-
-	"github.com/mattes/migrate"
-	_ "github.com/mattes/migrate/database/postgres"
-	_ "github.com/mattes/migrate/source/file"
 )
-
-func init() {
-	mustGetenv := func(key string) string {
-		val := os.Getenv(key)
-		if val == "" {
-			log.Fatalf("test: env: unable to find %q", key)
-		}
-		return val
-	}
-
-	m, err := migrate.New(mustGetenv("DB_MIGRATIONS"), mustGetenv("DB_URL"))
-	if err != nil {
-		log.Fatalf("test: create migration: %v", err)
-	}
-
-	if err := m.Up(); err != nil {
-		log.Fatalf("test: database up: %v", err)
-	}
-}
 
 func withDB(t *testing.T, fn func(*sql.DB) error) {
 	dbURL := os.Getenv("DB_URL")
